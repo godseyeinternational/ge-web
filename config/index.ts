2a375bf142f00365b1979465344d30f521932837
IncasedDevelopment <64618520+IncasedDevelopment@users.noreply.github.com>
2025-01-15 23:40:59 +1300

    image update

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 export const siteConfig: Metadata = {
   title: "GodsEye International",
-  description: "Welcome to my full stack Next.js 14 space portfolio.",
+  description: "GE.",
   keywords: [
     "reactjs",
     "nextjs",
