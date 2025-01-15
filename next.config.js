f84aac3d44d304be3ab6da0d8915520c3444b273
IncasedDevelopment <64618520+IncasedDevelopment@users.noreply.github.com>
2025-01-16 11:23:13 +1300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+const { DeleteSourceMapsPlugin } = require('webpack-delete-sourcemaps-plugin');
+module.exports = nextConfig 
 
-module.exports = nextConfig
+{
+  // ...
+  webpack: (config, { isServer }) => {
+    devtool: 'hidden-source-map', // optional, see the #hidden-source-map section for 
+    config.plugins.push(new DeleteSourceMapsPlugin({ isServer, keepServerSourcemaps: true }))
+    return config
+  }
+}
