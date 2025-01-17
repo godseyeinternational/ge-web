4c287f3023b013bdbc4cba887776d8a5b9ff63b3
IncasedDevelopment <64618520+IncasedDevelopment@users.noreply.github.com>
2025-01-17 20:15:46 +1300

    funding and meta edit

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 export const siteConfig: Metadata = {
   title: "GodsEye International",
-  description: "GE.",
+  description: "GE | GodsEye demonstrates the vulnerabilities of the digital world. Specializing in advanced information retrieval and digital access, we uncover the hidden truths behind modern privacy and security.",
   keywords: [
     "reactjs",
     "nextjs",
