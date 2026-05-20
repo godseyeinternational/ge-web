324e03b4df96c06a1c15e35b4686f3ede6270aa4
zwicz <godseyeinternational@proton.me>
2026-05-20 15:42:27 +1200

    Update navbar.tsx

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -48,7 +48,7 @@ export const Navbar = () => {
               rel="noreferrer noopener"
               className="cursor-pointer hover:text-[rgb(112,66,248)] transition"
             >
-              Gateway
+              Login
             </Link>
           </div>
         </div>
@@ -98,7 +98,7 @@ export const Navbar = () => {
               className="cursor-pointer hover:text-[rgb(112,66,248)] transition text-center"
               onClick={() => setIsMobileMenuOpen(false)}
             >
-              Gateway
+              Login
             </Link>
           </div>
 
